458c0de61cdd04f5852f497a3acf9598aec374fe
Amrit <linux.amrit@gmail.com>
2026-01-03 18:22:44 +0530

    Some imp rectifications

--- emp_mgmt_frontend/docker-entrypoint.sh
+++ emp_mgmt_frontend/docker-entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 set -eux
 
-: "${BACKEND_SERVICE_URL:=http://localhost:8080}"
+: "${BACKEND_API_BASE_URI:=/api}"
+: "${BACKEND_SERVICE_URL:=http://emp-mgmt-backend-service:8080}"
 
+export BACKEND_API_BASE_URI
+export BACKEND_SERVICE_URL
+
+# Update env.js for React App
 cat > /usr/share/nginx/html/env.js <<EOF
 window.__ENV__ = {
-  BACKEND_SERVICE_URL: "${BACKEND_SERVICE_URL}"
+  BACKEND_API_BASE_URI: "${BACKEND_API_BASE_URI}"
 };
 EOF
 
 # Generate Nginx config from template
+# We only want to substitute BACKEND_SERVICE_URL
 envsubst '${BACKEND_SERVICE_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
