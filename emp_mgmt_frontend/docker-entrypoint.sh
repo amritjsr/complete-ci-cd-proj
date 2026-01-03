68bcd9e9448d56d5ec23b42278608a2f8f4007de
Amrit <linux.amrit@gmail.com>
2026-01-03 18:10:08 +0530

    Some imp rectifications

--- emp_mgmt_frontend/docker-entrypoint.sh
+++ emp_mgmt_frontend/docker-entrypoint.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 set -eux
 
-: "${BACKEND_API_BASE_URL:=http://localhost:8080/api}"
+: "${BACKEND_SERVICE_URL:=http://localhost:8080}"
 
 cat > /usr/share/nginx/html/env.js <<EOF
 window.__ENV__ = {
-  BACKEND_API_BASE_URL: "${BACKEND_API_BASE_URL}"
+  BACKEND_SERVICE_URL: "${BACKEND_SERVICE_URL}"
 };
 EOF
 
 # Generate Nginx config from template
-envsubst '${BACKEND_API_BASE_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-echo "/etc/nginx/conf.d/default.conf.template"
-cat /etc/nginx/conf.d/default.conf.template
-
-echo "/etc/nginx/conf.d/default.conf"
-cat /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_SERVICE_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
--- emp_mgmt_frontend/src/api/employeeApi.js
+++ emp_mgmt_frontend/src/api/employeeApi.js
@@ -1,18 +1,18 @@
-// const BACKEND_API_BASE_URL =
-//   (typeof window !== "undefined" && window.__ENV && window.__ENV.BACKEND_API_BASE_URL) ||
-//   process.env.BACKEND_API_BASE_URL ||
+// const BACKEND_API_BASE_URI =
+//   (typeof window !== "undefined" && window.__ENV && window.__ENV.BACKEND_API_BASE_URI) ||
+//   process.env.BACKEND_API_BASE_URI ||
 //   "http://localhost:8080/api";
 
-const BACKEND_API_BASE_URL = "/api";
-console.log(BACKEND_API_BASE_URL);
+const BACKEND_API_BASE_URI = "/api";
+
 
 export async function getEmployees() {
-  const response = await fetch(`${BACKEND_API_BASE_URL}/employees/`);
+  const response = await fetch(`${BACKEND_API_BASE_URI}/employees/`);
   return response.json();
 }
 
 export async function createEmployee(employee) {
-  const response = await fetch(`${BACKEND_API_BASE_URL}/employees/`, {
+  const response = await fetch(`${BACKEND_API_BASE_URI}/employees/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(employee),
@@ -21,14 +21,14 @@ export async function createEmployee(employee) {
 }
 
 export async function deleteEmployee(user_name) {
-  await fetch(`${BACKEND_API_BASE_URL}/employees/${user_name}/update/`, {
+  await fetch(`${BACKEND_API_BASE_URI}/employees/${user_name}/update/`, {
     method: "DELETE",
   });
 }
 
 export async function checkHealth() {
   try {
-    const res = await fetch(`${BACKEND_API_BASE_URL}/health/`);
+    const res = await fetch(`${BACKEND_API_BASE_URI}/health/`);
     return res.ok;
   } catch (e) {
     return false;
