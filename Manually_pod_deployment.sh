b21196105ba6bfd308e4313af618e8d7b001cc4e
Amrit <linux.amrit@gmail.com>
2026-01-03 13:18:52 +0530

    Some imp rectifications

--- Manually_pod_deployment.sh
+++ Manually_pod_deployment.sh
@@ -9,14 +9,14 @@ sleep 5
 kubectl delete -f backend-services.yaml
 sleep 5
 kubectl delete -f frontend-service.yaml
-
+sleep 10
 
 kubectl apply -f config-maps.yaml
 kubectl apply -f secrets.yaml
 kubectl apply -f backend-deployment.yaml
 sleep 5
-kubectl apply -f frontend-deployments.yaml
-sleep 5
 kubectl apply -f backend-services.yaml
+sleep 5
+kubectl apply -f frontend-deployments.yaml
 sleep 5
 kubectl apply -f frontend-service.yaml
